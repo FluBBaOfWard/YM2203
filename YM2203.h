b58c60559ffdda1e303c82705d085bbc099a06e7
Fredrik Ahlström <fredrikahlstrom75@gmail.com>
2024-02-11 22:12:47 +0100

    Fixed C header some more.

--- YM2203.h
+++ YM2203.h
@@ -6,6 +6,10 @@
 
 #include "AY38910/AY38910.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef struct {
 	AY38910 ay38910chip;
 
@@ -14,12 +18,16 @@ typedef struct {
 
 } YM2203;
 
-void ym2203Reset(void *irqFunc, YM2203 *chip);
+void ym2203Reset(YM2203 *chip, void *irqFunc);
 void ym2203Mixer(int len, void *dest, YM2203 *chip);
+void ym2203Run(u8 cycles, YM2203 *chip);
 void ym2203IndexW(u8 value, YM2203 *chip);
 void ym2203DataW(u8 value, YM2203 *chip);
-void ym2203StatusR(YM2203 *chip);
-void ym2203DataR(YM2203 *chip);
+u8 ym2203StatusR(YM2203 *chip);
+u8 ym2203DataR(YM2203 *chip);
 
-
+#ifdef __cplusplus
+} // extern "C"
 #endif
+
+#endif // ARMYM2203_HEADER
